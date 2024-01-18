7b1691b62b7cec40d52ce2d9d2f9a2bde4930d6a
韩国坤 <2663968138@qq.com>
2024-01-18 15:12:27 +0800

    Create

---
+++ src/assets/demo.js
@@ -0,0 +1,38 @@
+
+
+window.onload = function () {
+    showCpuInfo();
+    setInterval(() => {
+        showCpuInfo();
+    }, 1000);
+}
+
+
+function showCpuInfo() {
+    var cpu = this.document.querySelector('#cpu');
+    var mem = this.document.querySelector('#mem');
+    var upload = this.document.querySelector('#upload');
+    var download = this.document.querySelector('#download');
+    var port = this.document.querySelector('#port');
+    var ipaddr = this.document.querySelector('#ipaddr');
+    var ajax = new XMLHttpRequest();
+    ajax.open('get', 'data');
+    ajax.send();
+    ajax.onerror = function (error) {
+        // 请求出现错误
+        alert('出现异常：', JSON.stringify(error));
+    };
+    ajax.onreadystatechange = function () {
+        if (ajax.response) {
+            console.log(ajax.response);
+            var data = JSON.parse(ajax.response);
+            cpu.innerHTML = data.cpu;
+            mem.innerHTML = data.mem;
+            upload.innerHTML = data.upload;
+            download.innerHTML = data.download;
+            port.innerHTML = data.port;
+            ipaddr.innerHTML = data.ipaddr;
+        }
+    };
+}
+
